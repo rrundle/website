c4c751b518f003da64098deaaf284e5f2ac53842
Ryan Rundle <ryan.rundle@ymail.com>
2017-03-01 11:18:19 -0800

    Updated scroll animation and deploy files, #15.

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 document.addEventListener('click', function(e) {
   if (e.target.id.indexOf('about') !== -1) {
     var about = document.querySelector('.about')
-    about.scrollIntoView({
-      behavior: 'smooth'
-    })
+    zenscroll.to(about, 1000)
   }
   if (e.target.id.indexOf('projects') !== -1) {
     var projects = document.querySelector('.projects')
-    projects.scrollIntoView({
-      behavior: 'smooth'
-    })
+    zenscroll.to(projects, 1000)
   }
   if (e.target.id.indexOf('contact') !== -1) {
     var contact = document.querySelector('.contact')
-    contact.scrollIntoView({
-      behavior: 'smooth'
-    })
+    zenscroll.to(contact, 1000)
   }
 })
