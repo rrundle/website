4401162bee3af545bfc852ab2e9d1e09fe59179e
Ryan Rundle <ryan.rundle@ymail.com>
2017-07-02 13:25:54 -0700

    Editing package.json for deployment.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { PORT } = process.env
+const router = require('./router')
+
+express()
+  .use(express.static(`${__dirname}/public`))
+  .use(bodyParser.json())
+  .use('/notes', router)
+  .listen(PORT || 3001, () => {
+    console.log(`Listening on port ${PORT || 3001}`) // eslint-disable-line
+  })
