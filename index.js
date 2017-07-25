ec3c0b7dbb221df02ba362a1de7c6791f6fe4e56
Ryan Rundle <ryan.rundle@ymail.com>
2017-07-24 21:16:43 -0700

    Restructuring file system

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const { PORT } = process.env
-
-app.use(express.static(`${__dirname}/public`))
-app.use(bodyParser.json())
-app.listen(PORT || 3001, (req, res) => console.log(`Listening on port: ${PORT || 3001}`))
