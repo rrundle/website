6370ef225e41cb81454f2aec6a7efb79b6e3d4bb
Ryan Rundle <ryan.rundle@verys.com>
2020-04-03 10:09:02 -0700

    change email address link

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles.css'
 
+// eslint-disable-next-line react/prop-types
 const Navbar = ({ light }) => (
   <div className={`navbar-wrapper ${light && 'navbar-wrapper-on'}`}>
     {light && (
@@ -8,7 +9,7 @@ const Navbar = ({ light }) => (
         <a href="https://www.linkedin.com/in/ryanrundle/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         <a href="https://github.com/rrundle" target="_blank" rel="noopener noreferrer">GitHub</a>
         {/* <a href="/#">Resume</a> */}
-        <a href="mailto:ryan@ryanrundle.com" target="_blank" rel="noopener noreferrer">Contact</a>
+        <a href="mailto:ryan.rundle@ymail.com" target="_blank" rel="noopener noreferrer">Contact</a>
       </nav>
     )}
   </div>
